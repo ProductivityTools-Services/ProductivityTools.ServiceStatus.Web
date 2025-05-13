88ada096cbeb8057a1806a4b1a66f1fd10421d04
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-13 18:37:01 +0200

    change address

--- src/Config.js
+++ src/Config.js
@@ -1,11 +1,11 @@
 
 const dev = {
-    configUrls : 'https://script.google.com/macros/s/AKfycbzfyaODd00E7rwh4PsTwA3lgavnW_CDbtbE1agFULnAOPDJn7vWqsQz-nKHjQzc9J4W/exec'
+    configUrls : 'https://script.google.com/macros/s/AKfycbx7ieTYTvIpJAO2DQ9zjtGG_jdCW41vh9tD8k1ozzPiCGPhBdI6XtEU70nxHj_GFZDL/exec'
 }
 
 
 const prod = {
-    configUrls : 'https://script.google.com/macros/s/AKfycbzfyaODd00E7rwh4PsTwA3lgavnW_CDbtbE1agFULnAOPDJn7vWqsQz-nKHjQzc9J4W/exec'
+    configUrls : 'https://script.google.com/macros/s/AKfycbx7ieTYTvIpJAO2DQ9zjtGG_jdCW41vh9tD8k1ozzPiCGPhBdI6XtEU70nxHj_GFZDL/exec'
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
